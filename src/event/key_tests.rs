68aaa21030c86d2805929dc58a7acfe0d9ebe208
Dark-Alex-17 <alex.j.tusa@gmail.com>
2023-08-08 10:50:07 -0600

    Changed Ctrl-r to ctrl-r for displaying key mappings

--- src/event/key_tests.rs
+++ src/event/key_tests.rs
@@ -19,7 +19,7 @@ mod tests {
   #[case(Key::Tab, "tab")]
   #[case(Key::Delete, "del")]
   #[case(Key::Char('q'), "q")]
-  #[case(Key::Ctrl('q'), "Ctrl-q")]
+  #[case(Key::Ctrl('q'), "ctrl-q")]
   fn test_key_formatter(#[case] key: Key, #[case] expected_str: &str) {
     assert_str_eq!(format!("{}", key), format!("<{}>", expected_str));
   }
