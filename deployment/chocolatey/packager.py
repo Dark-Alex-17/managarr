4f6e64083a27301e6edfddd054631d985e45973b
Alex Clarke <alex.j.tusa@gmail.com>
2024-12-19 16:36:56 -0700

    ci: Explicitly set the encoding as UTF-8 in the python script since that is not the windows default encoding [skip ci]

--- deployment/chocolatey/packager.py
+++ deployment/chocolatey/packager.py
@@ -16,12 +16,12 @@ print("     TEMPLATE PATH: %s" % template_file_path)
 print("     SAVING AT: %s" % generated_file_path)
 print("     HASH: %s" % hash_64)
 
-with open(template_file_path, "r") as template_file:
+with open(template_file_path, "r", encoding="utf-8") as template_file:
     template = Template(template_file.read())
     substitute = template.safe_substitute(version=version, hash_64=hash_64)
     print("\n================== Generated package file ==================\n")
     print(substitute)
     print("\n============================================================\n")
 
-    with open(generated_file_path, "w") as generated_file:
+    with open(generated_file_path, "w", encoding="utf-8") as generated_file:
         generated_file.write(substitute)
